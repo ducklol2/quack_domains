a26ac6cf4c61a3dbb5f34e58ea89dd59465bde01
ducklol2 <140205571+ducklol2@users.noreply.github.com>
2023-09-11 18:50:10 +0200

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+function _term {
+  pkill -P $$
+}
+
+trap _term SIGTERM
+
 avahi-publish-address -R jfin.macmini.local 192.168.86.27 &
+
+echo 'Publishing.'
 
 while true; do sleep 10000; done
