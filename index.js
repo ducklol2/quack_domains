9526e28e436cfd980b423635c9b0d75d4161624c
ducklol2 <140205571+ducklol2@users.noreply.github.com>
2023-10-07 09:38:32 +0000

    clean up js a bit

--- index.js
+++ index.js
@@ -2,73 +2,97 @@ const { request } = require('http');
 const { spawn } = require('child_process');
 const { networkInterfaces } = require('os');
 
-const traefikRuleRegex = /traefik.http.routers.(\w+).rule/;
-const traefikHostRegex = /Host\(`([\w\.]+\.local)`\)/;
+const TRAEFIK_RULE_REGEX = /traefik.http.routers.(\w+).rule/;
+const TRAEFIK_HOST_REGEX = /Host\(`([\w\.]+\.local)`\)/;
+const REQUEST_OPTIONS = {
+  socketPath: '/run/docker.sock',
+  path: 'http://localhost/containers/json',
+};
 
 main();
 
-function main() {
-  const ip = getIp();
-  console.log(`Using IP: ${ip}`);
+async function main() {
+  const hostIp = getIp();
+  if (!hostIp) {
+    console.log('No potential IPs found :( (TODO: Allow user to set one)');
+    return;
+  }
 
-  const options = {
-    socketPath: '/run/docker.sock',
-    path: 'http://localhost/containers/json',
-  };
-
-  const callback = res => {
-    console.log(`STATUS: ${res.statusCode}`);
-    res.setEncoding('utf8');
-    res.on('data', data => publishContainers(JSON.parse(data), ip));
-    res.on('error', data => console.error(data));
-  };
-
-  const clientRequest = request(options, callback);
-  clientRequest.end();
+  const newContainerHosts = await getHostsAndPublish(hostIp);
 }
 
-// Guesses the local machine's local IP.
+// Guesses the local host machine's local IP.
 function getIp() {
-  let ip;
+  const ips = [];
   for (const [interface, networks] of Object.entries(networkInterfaces())) {
     for (const network of networks) {
+      // Skip internal networks.
       if (network.internal) continue;
-      if (network.family != 'IPv4') continue;  // may be 4 in some node versions?
-      console.log(`found interface ${interface} with ipv4 ${network.address}, family ${network.family}`);
-      if (!ip) ip = network.address;  // just use first one, but list 'em all
+
+      // Skip IPv6 networks. (TODO: Support & test them)
+      if (network.family != 'IPv4' && network.family != 4) continue;
+
+      // Skip "br-..." networks, they're probably Docker bridge networks?
+      if (interface.startsWith('br-')) continue;
+
+      console.log(`Found interface ${interface} with ipv4 ${network.address}, family ${network.family}`);
+
+      ips.push(network.address);
     }
   }
-  return ip;
+
+  if (!ips.length) return null;
+
+  console.log(`Using the first address found, ${ips[0]}.`);
+  return ips[0];
 }
 
-// Given the Docker container JSON list, finds matching
-// Traefik router labels with Host() rules and publishes them for the IP.
-function publishContainers(containers, ip) {
-  for (const container of containers) {
-    console.log(`container id ${container['Id']}`);
+// Finds Traefik router labels with matching Host() rules.
+async function getHostsAndPublish(hostIp) {
+  const containers = await getDockerContainers();
 
-    for (const [label, value] of Object.entries(container['Labels'])) {
-      const labelMatch = traefikRuleRegex.exec(label);
-      if (labelMatch) {
-        const rules = value.split('||');
-        for (const rule of rules) {
-          const valueMatch = traefikHostRegex.exec(rule);
-          if (valueMatch) {
-            console.log(`container router host: ${valueMatch[1]}`);
-            publish(valueMatch[1], ip);
-          }
-        }
+  const containerHosts = {};
+  for (const container of containers) {
+    const containerName = container['Names'][0];
+    containerHosts[containerName] = [];
+
+    for (const [label, labelValue] of Object.entries(container['Labels'])) {
+      const labelMatch = TRAEFIK_RULE_REGEX.exec(label);
+      if (!labelMatch) continue;
+
+      for (const rule of labelValue.split('||')) {
+        const valueMatch = TRAEFIK_HOST_REGEX.exec(rule);
+        if (!valueMatch) continue;
+        
+        const host = valueMatch[1];
+        containerHosts[containerName][host] = publish(host, hostIp);
       }
     }
   }
+
+  return containerHosts;
 }
 
+// Lists all Docker containers from the Docker socket.
+async function getDockerContainers() {
+  return new Promise((resolve, reject) => {
+    request(REQUEST_OPTIONS, response => {
+      response.setEncoding('utf8');
+      response.on('data', data => resolve(JSON.parse(data)));
+      response.on('error', error => reject(error));
+    }).end();
+  });
+}
+
+// Runs avahi-publish-address.
 function publish(host, ip) {
   console.log(`Running avahi-publish-address for host ${host} pointing to IP ${ip}`);
-  const avahi = spawn('avahi-publish-address', ['-R', host, ip]);
 
-  avahi.stdout.on("data", data => console.log(`avahi stdout: ${data}`));
-  avahi.stderr.on("data", data => console.log(`avahi stderr: ${data}`));
-  avahi.on('error', err => console.log('avahi err: ', err));
-  avahi.on('close', code => console.log('avahi close: ', code));
+  const process = spawn('avahi-publish-address', ['-R', host, ip]);
+  process.stdout.on('data', data => console.log(`Avahi stdout: ${data.toString().trim()}`));
+  process.stderr.on('data', data => console.log(`Avahi stderr: ${data.toString().trim()}`));
+  process.on('error', err => console.error('Avahi err:', err));
+  process.on('close', code => console.log('Avahi close:', code));
+
+  return process;
 }
